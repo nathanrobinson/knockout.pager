281f3c1fa4f946b2dfaaae7905b9b6bea40436d8
nathanrobinson <nathanrobinson@yahoo.com>
2013-08-28 16:33:06 -0500

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,47 +1,46 @@
 (function ($, ko){
     $(function () {
         module("paging view model");
-        test("inital pages are empty", function () {
-          var observableArray = ko.observableArray([]);
-          
+        
+            var observableArray = ko.observableArray([]);
             var pager = new ko.bindingHandlers.pagedForeach.Pager(observableArray);
-            
+          
+        test("inital pages are empty", function () {
             QUnit.deepEqual(pager.pagedItems(), [], "paged items");
         });
         test("inital page is 1", function () {
-          var observableArray = ko.observableArray([]);
-          
-            var pager = new ko.bindingHandlers.pagedForeach.Pager(observableArray);
-            
             QUnit.equal(pager.page(), 1, "page");
         });
         test("inital itemsPerPage is 10", function () {
-          var observableArray = ko.observableArray([]);
-          
-            var pager = new ko.bindingHandlers.pagedForeach.Pager(observableArray);
-            
             QUnit.equal(pager.itemsPerPage(), 10, "itemsPerPage");
         });
         test("inital allowChangePageSize is false", function () {
-          var observableArray = ko.observableArray([]);
-          
-            var pager = new ko.bindingHandlers.pagedForeach.Pager(observableArray);
-            
             QUnit.equal(pager.allowChangePageSize(), false, "allowChangePageSize");
         });
         test("inital totalPages is 0", function () {
-          var observableArray = ko.observableArray([]);
-          
-            var pager = new ko.bindingHandlers.pagedForeach.Pager(observableArray);
-            
             QUnit.equal(pager.totalPages(), 0, "totalPages");
         });
         test("inital relativePages are empty", function () {
-          var observableArray = ko.observableArray([]);
-          
-            var pager = new ko.bindingHandlers.pagedForeach.Pager(observableArray);
-            
             QUnit.deepEqual(pager.relativePages(), [], "relativePages");
+        });
+        test("pager updates with observableArray", function(){
+            var array = [0, 1, 2, 3, 4, 5];
+            observableArray(array);
+            QUnit.deepEqual(pager.pagedItems(), array, "paged items");
+        });
+        test("page cannot go below 1", function () {
+            expect(2);
+            pager.page(0);
+            QUnit.equal(pager.page(), 1, "page");
+            pager.page(-1);
+            QUnit.equal(pager.page(), 1, "page");
+        });
+        test("page cannot go past last page", function () {
+            expect(2);
+            pager.page(2);
+            QUnit.equal(pager.page(), 1, "page");
+            pager.page(10);
+            QUnit.equal(pager.page(), 1, "page");
         });
     });
 }(jQuery, ko));
