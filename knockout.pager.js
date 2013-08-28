574147d033763d6730850b983a0001b412c2abdc
nathanrobinson <nathanrobinson@yahoo.com>
2013-08-28 15:23:26 -0500

    Update knockout.pager.js
    Added ability to control page size either through binding or user interaction.

--- knockout.pager.js
+++ knockout.pager.js
@@ -1,49 +1,5 @@
-(function (ko){
+(function (ko) {
 
-    function Pager(observableArray){
-        var self = this;
-
-        self.page = ko.observable(1);
-        
-        observableArray.subscribe(function () {
-            self.page(1);
-        });
-        
-        self.page.subscribe(function (newVal) {
-            var n = (newVal + '').replace(/[^0-9]/g, '');
-            if (n < 1) n = 1;
-            else if (n > self.totalPages()) n = self.totalPages();
-            if (n != newVal) {
-                self.page(n);
-            }
-        });
-
-        var array = observableArray();
-
-        self.totalPages = ko.computed(function () {
-            var array = observableArray();
-            return Math.ceil(array.length / 10);
-        });
-        self.pagedItems = ko.computed(function () {
-            var array = observableArray();
-            var indexOfFirstItemOnCurrentPage = (((self.page() * 1) - 1) * 10);
-            var pageArray = array.slice(indexOfFirstItemOnCurrentPage, indexOfFirstItemOnCurrentPage + 10);
-            return pageArray;
-        });
-
-        self.relativePages = ko.computed(function () {
-            var currentPage = self.page() * 1;
-            var totalPages = self.totalPages();
-            var pagesFromEnd = totalPages - currentPage;
-            var extraPagesAtFront = Math.max(0, 2 - pagesFromEnd);
-            var extraPagesAtEnd = Math.max(0, 3 - currentPage);
-            var firstPage = Math.max(1, currentPage - (2 + extraPagesAtFront));
-            var lastPage = Math.min(self.totalPages(), currentPage + (2 + extraPagesAtEnd));
-
-            return ko.utils.range(firstPage, lastPage);
-        });
-    }
-    
     // Template used to render the page links
     var templateEngine = new ko.nativeTemplateEngine();
 
@@ -61,36 +17,143 @@
         </div>\
     ");
 
+    templateEngine.addTemplate("ko_pager_size", "\
+        <div class='pager-size' data-bind='if: allowChangePageSize'>\
+            <select data-bind='value: itemsPerPage'>\
+                <option>10</option>\
+                <option>25</option>\
+                <option>50</option>\
+                <option>100</option>\
+            </select>\
+        </div>\
+    ");
+
     function makeTemplateValueAccessor(pager) {
-        return function (){
-            return { 'foreach' : pager.pagedItems, 'templateEngine' : templateEngine };
+        return function () {
+            return { 'foreach': pager.pagedItems, 'templateEngine': templateEngine };
         };
+    }
+    
+    function defaultPagerIfEmpty(observable) {
+        if (!observable.pager) observable.pager = new ko.bindingHandlers.pagedForeach.Pager(observable);
+    }
+
+    function checkItemPerPageBinding(allBindings, pager){
+        if (allBindings['itemsPerPage']) {
+            if (ko.isObservable(allBindings['itemsPerPage'])) {
+                allBindings['itemsPerPage'].subscribe(function (newVal) {
+                    pager.itemsPerPage(newVal);
+                });
+                pager.itemsPerPage.subscribe(function (newVal) {
+                    allBindings['itemsPerPage'](newVal);
+                });
+            }
+            else {
+                pager.itemsPerPage(allBindings['itemsPerPage']);
+            }
+        }
     }
 
     ko.bindingHandlers.pagedForeach = {
+        Pager : function (observableArray){
+            var self = this;
+
+            self.page = ko.observable(1);
+
+            self.itemsPerPage = ko.observable(10);
+            self.allowChangePageSize = ko.observable(false);
+
+            self.totalPages = ko.computed(function () {
+                var array = observableArray();
+                return Math.ceil(array.length / self.itemsPerPage());
+            });
+            self.pagedItems = ko.computed(function () {
+                var array = observableArray();
+                var indexOfFirstItemOnCurrentPage = (((self.page() * 1) - 1) * (self.itemsPerPage() * 1));
+                var pageArray = array.slice(indexOfFirstItemOnCurrentPage, indexOfFirstItemOnCurrentPage + (self.itemsPerPage()* 1));
+                return pageArray;
+            });
+
+            self.relativePages = ko.computed(function () {
+                var currentPage = self.page() * 1;
+                var totalPages = self.totalPages();
+                var pagesFromEnd = totalPages - currentPage;
+                var extraPagesAtFront = Math.max(0, 2 - pagesFromEnd);
+                var extraPagesAtEnd = Math.max(0, 3 - currentPage);
+                var firstPage = Math.max(1, currentPage - (2 + extraPagesAtFront));
+                var lastPage = Math.min(self.totalPages(), currentPage + (2 + extraPagesAtEnd));
+
+                return ko.utils.range(firstPage, lastPage);
+            });
+
+            observableArray.subscribe(function () {
+                self.page(1);
+            });
+
+            self.itemsPerPage.subscribe(function () {
+                self.page(1);
+            });
+
+            self.page.subscribe(function (newVal) {
+                var n = (newVal + '').replace(/[^0-9]/g, '');
+                if (n < 1) n = 1;
+                else if (n > self.totalPages()) n = self.totalPages();
+                if (n != newVal) {
+                    self.page(n);
+                }
+            });
+
+            return self;
+        },
         init : function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
-            var observable = valueAccessor();
-            if (!observable.pager) observable.pager = new Pager(observable);
+            var observable = valueAccessor(), allBindings = allBindingsAccessor();
+            defaultPagerIfEmpty(observable);
+            checkItemPerPageBinding(allBindings, observable.pager);
             var array = ko.utils.unwrapObservable(observable);
             return ko.bindingHandlers.template.init(element, makeTemplateValueAccessor(observable.pager));
         },
         update : function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
             var observable = valueAccessor();
             var array = ko.utils.unwrapObservable(observable);
-            if (!observable.pager) observable.pager = new Pager(observable);
+            defaultPagerIfEmpty(observable);
             return ko.bindingHandlers.template.update(element, makeTemplateValueAccessor(observable.pager), allBindingsAccessor, viewModel, bindingContext);
         }
     };
 
-    ko.bindingHandlers.pageLinks = {
-        init : function (){
-            return { 'controlsDescendantBindings' : true };
+    ko.bindingHandlers.pageSize = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var observable = valueAccessor(), allBindings = allBindingsAccessor();
+            defaultPagerIfEmpty(observable);
+            checkItemPerPageBinding(allBindings, observable.pager);
+            return { 'controlsDescendantBindings': true };
         },
-        update : function (element, viewModelAccessor, allBindingsAccessor){
-            var observable = viewModelAccessor(), allBindings = allBindingsAccessor();
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var observable = valueAccessor();
             var array = ko.utils.unwrapObservable(observable);
-            if (!observable.pager) observable.pager = new Pager(observable);
+            defaultPagerIfEmpty(observable);
+            
+            observable.pager.allowChangePageSize(true);
+            
+            // Empty the element
+            while (element.firstChild) ko.removeNode(element.firstChild);
 
+            // Render the page links
+            ko.renderTemplate('ko_pager_size', observable.pager, { templateEngine: templateEngine }, element, "replaceNode");
+        }
+    };
+
+    ko.bindingHandlers.pageLinks = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var observable = valueAccessor(), allBindings = allBindingsAccessor();
+            defaultPagerIfEmpty(observable);
+            checkItemPerPageBinding(allBindings, observable.pager);
+            return { 'controlsDescendantBindings': true };
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var observable = valueAccessor();
+            var array = ko.utils.unwrapObservable(observable);
+            defaultPagerIfEmpty(observable);
+            
             // Empty the element
             while (element.firstChild) ko.removeNode(element.firstChild);
 
