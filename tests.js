4f270cb2683b078b8b7423a9efa00768a5cfc180
nathanrobinson <nathanrobinson@yahoo.com>
2013-09-06 12:51:42 -0500

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,21 +1,5 @@
 (function ($, ko){
     $(function () {
-        var array = [], array2 = [];
-        for(var c=1;c<1000;c++){
-            array.push( { key: 'key_' + c, value: 'value ' + c } );
-            array2.push( { key: 'key_' + c, value: 'value ' + c } );
-        }
-        var viewModel = { 
-            observableArray: ko.observableArray(array),
-            pageSize: ko.observable(25)
-        };
-        ko.applyBindings(viewModel, $('#testBinding')[0]);
-        
-        var viewModel2 = { 
-            observableArray: array2,
-            pageSize: ko.observable(10)
-        };
-        ko.applyBindings(viewModel2, $('#testBinding2')[0]);
         
         module("paging view model - empty initializer");
             var observableArray = ko.observableArray([]);
@@ -103,59 +87,119 @@
         });
         
         
-        module("pagingForeach binding");
+        module("pagingForeach binding - observableArray");
+        
+        var array = [], array2 = [];
+        for(var c=1;c<1000;c++){
+            array.push( { key: 'key_' + c, value: 'value ' + c } );
+            array2.push( { key: 'key_' + c, value: 'value ' + c } );
+        }
+        var viewModel = { 
+            observableArray: ko.observableArray(array),
+            pageSize: ko.observable(25)
+        };
+        ko.applyBindings(viewModel, $('#testBinding')[0]);
         
         test("pageSizeControl binding adds a select with 4 options", function () {
             expect(2);
-            QUnit.equal($('#testBinding').find('div.pager-size').children('select').length, 1, "page");
-            QUnit.equal($('#testBinding').find('div.pager-size').children('select').children('option').length, 4, "page");
+            QUnit.equal($('#testBinding').find('div.pager-size').children('select').length, 1, "pageSizeControl");
+            QUnit.equal($('#testBinding').find('div.pager-size').children('select').children('option').length, 4, "pageSizeControl");
         });
         test("pageSize binding adds 25 rows", function () {
-            QUnit.equal($('#testBinding').find('tbody').children('tr').length, 25, "page");
+            QUnit.equal($('#testBinding').find('tbody').children('tr').length, 25, "pageSize");
         });
         test("pageSize binding updates to 10 rows", function () {
             viewModel.pageSize(10);
-            QUnit.equal($('#testBinding').find('tbody').children('tr').length, 10, "page");
+            QUnit.equal($('#testBinding').find('tbody').children('tr').length, 10, "pageSize");
         });
         test("pageLinks adds first page button", function () {
-            QUnit.equal($('#testBinding').find('.first-page-link').children('a').length, 1, "page");
+            QUnit.equal($('#testBinding').find('.first-page-link').children('a').length, 1, "pageLinks");
         });
         test("pageLinks adds last page button", function () {
-            QUnit.equal($('#testBinding').find('.last-page-link').children('a').length, 1, "page");
+            QUnit.equal($('#testBinding').find('.last-page-link').children('a').length, 1, "pageLinks");
         });
         test("pageLinks adds page number buttons", function () {
-            QUnit.equal($('#testBinding').find('.pager-pages').children('.pager-page').children('a').length, 5, "page");
+            QUnit.equal($('#testBinding').find('.pager-pages').children('.pager-page').children('a').length, 5, "pageLinks");
         });
         test("first page button is disabled on page 1", function () {
-            QUnit.equal($('#testBinding').find('.first-page-link').children('a').hasClass('disabled'), true, "page");
+            QUnit.equal($('#testBinding').find('.first-page-link').children('a').hasClass('disabled'), true, "pageLinks");
         });
         test("first page button is not disabled on other pages", function () {
             viewModel.observableArray.pager.page(2);
-            QUnit.equal($('#testBinding').find('.first-page-link').children('a').hasClass('disabled'), false, "page");
-        });
-        test("can update page size to 25", function () {
-            expect(2)
-            viewModel.pageSize(25);
-            QUnit.equal($('#testBinding').find('tbody').children('tr').length, 25, "page");
-            viewModel.pageSize(10);
-            QUnit.equal($('#testBinding').find('tbody').children('tr').length, 10, "page");
+            QUnit.equal($('#testBinding').find('.first-page-link').children('a').hasClass('disabled'), false, "pageLinks");
         });
         test("last page button is disabled on last page", function () {
             viewModel.observableArray.pager.page(100);
-            QUnit.equal($('#testBinding').find('.last-page-link').children('a').hasClass('disabled'), true, "page");
+            QUnit.equal($('#testBinding').find('.last-page-link').children('a').hasClass('disabled'), true, "last page");
         });
         test("last page button is not disabled on other pages", function () {
             viewModel.observableArray.pager.page(99);
-            QUnit.equal($('#testBinding').find('.last-page-link').children('a').hasClass('disabled'), false, "page");
+            QUnit.equal($('#testBinding').find('.last-page-link').children('a').hasClass('disabled'), false, "last page");
             viewModel.observableArray.pager.page(1);
         });
         test("$parent binding has correct scope", function () {
             expect(2)
             viewModel.pageSize(25);
-            QUnit.equal($('#testBinding').find('.parentPageSize').first().text(), 25, "page");
+            QUnit.equal($('#testBinding').find('.parentPageSize').first().text(), 25, "$parent");
             viewModel.pageSize(10);
-            QUnit.equal($('#testBinding').find('.parentPageSize').first().text(), 10, "page");
+            QUnit.equal($('#testBinding').find('.parentPageSize').first().text(), 10, "$parent");
         });
+        
+        module("pagingForeach binding - static array");
+        
+        var viewModel2 = { 
+            array: array2,
+            pageSize: ko.observable(10)
+        };
+        ko.applyBindings(viewModel2, $('#testBinding2')[0]);
+        
+        test("pageSizeControl binding adds a select with 4 options", function () {
+            expect(2);
+            QUnit.equal($('#testBinding2').find('div.pager-size').children('select').length, 1, "pageSizeControl");
+            QUnit.equal($('#testBinding2').find('div.pager-size').children('select').children('option').length, 4, "pageSizeControl");
+        });
+        test("pageSize binding adds 10 rows", function () {
+            QUnit.equal($('#testBinding2').find('tbody').children('tr').length, 10, "pageSize");
+        });
+        test("pageSize binding updates", function () {
+            viewModel.pageSize(25);
+            QUnit.equal($('#testBinding2').find('tbody').children('tr').length, 25, "pageSize");
+            viewModel.pageSize(10);
+            QUnit.equal($('#testBinding2').find('tbody').children('tr').length, 10, "pageSize");
+        });
+        test("pageLinks adds first page button", function () {
+            QUnit.equal($('#testBinding2').find('.first-page-link').children('a').length, 1, "pageLinks");
+        });
+        test("pageLinks adds last page button", function () {
+            QUnit.equal($('#testBinding2').find('.last-page-link').children('a').length, 1, "pageLinks");
+        });
+        test("pageLinks adds page number buttons", function () {
+            QUnit.equal($('#testBinding2').find('.pager-pages').children('.pager-page').children('a').length, 5, "pageLinks");
+        });
+        test("first page button is disabled on page 1", function () {
+            QUnit.equal($('#testBinding2').find('.first-page-link').children('a').hasClass('disabled'), true, "pageLinks");
+        });
+        test("first page button is not disabled on other pages", function () {
+            viewModel.array.pager.page(2);
+            QUnit.equal($('#testBinding2').find('.first-page-link').children('a').hasClass('disabled'), false, "pageLinks");
+        });
+        test("last page button is disabled on last page", function () {
+            viewModel.array.pager.page(100);
+            QUnit.equal($('#testBinding2').find('.last-page-link').children('a').hasClass('disabled'), true, "last page");
+        });
+        test("last page button is not disabled on other pages", function () {
+            viewModel.array.pager.page(99);
+            QUnit.equal($('#testBinding2').find('.last-page-link').children('a').hasClass('disabled'), false, "last page");
+            viewModel.array.pager.page(1);
+        });
+        test("$parent binding has correct scope", function () {
+            expect(2)
+            viewModel.pageSize(25);
+            QUnit.equal($('#testBinding2').find('.parentPageSize').first().text(), 25, "$parent");
+            viewModel.pageSize(10);
+            QUnit.equal($('#testBinding2').find('.parentPageSize').first().text(), 10, "$parent");
+        });
+        
         
     });
 }(jQuery, ko));
