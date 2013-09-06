64fc1fa041e6531dfb651ba5307aaa2be946b6af
nathanrobinson <nathanrobinson@yahoo.com>
2013-09-06 13:37:24 -0500

    Update knockout.pager.js

--- knockout.pager.js
+++ knockout.pager.js
@@ -18,7 +18,7 @@
     ");
 
     templateEngine.addTemplate("ko_pager_size", "\
-            <select class='pageSizeControl' data-bind='value: itemsPerPage, enable: allowChangePageSize'>\
+            <select class='pager-size' data-bind='value: itemsPerPage, enable: allowChangePageSize'>\
                 <option>10</option>\
                 <option>25</option>\
                 <option>50</option>\
