1b2cd285535d1aa824341f8d08c2110dd46c1f39
nathanrobinson <nathanrobinson@yahoo.com>
2013-09-07 15:50:26 -0500

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,9 +1,8 @@
 (function ($, ko){
     $(function () {
         
-        module("paging view model - empty initializer");
-            var observableArray = ko.observableArray([]);
-            var pager = new ko.bindingHandlers.pagedForeach.ClientPager(observableArray);
+        module("paging view model - base Pager - empty initializer");
+            var pager = new ko.bindingHandlers.pagedForeach.Pager();
         
         test("inital pages are empty", function () {
             QUnit.deepEqual(pager.pagedItems(), [], "paged items");
@@ -24,66 +23,89 @@
             QUnit.deepEqual(pager.relativePages(), [], "relativePages");
         });
         
+        module("paging view model - ClientPager - empty initializer");
+            var observableArray = ko.observableArray([]);
+            var clientPager = new ko.bindingHandlers.pagedForeach.ClientPager(observableArray);
         
-        module("paging view model - updates to observableArray");
+        test("inital pages are empty", function () {
+            QUnit.deepEqual(clientPager.pagedItems(), [], "paged items");
+        });
+        test("inital page is 1", function () {
+            QUnit.equal(clientPager.page(), 1, "page");
+        });
+        test("inital itemsPerPage is 10", function () {
+            QUnit.equal(clientPager.itemsPerPage(), 10, "itemsPerPage");
+        });
+        test("inital allowChangePageSize is false", function () {
+            QUnit.equal(clientPager.allowChangePageSize(), false, "allowChangePageSize");
+        });
+        test("inital totalPages is 0", function () {
+            QUnit.equal(clientPager.totalPages(), 0, "totalPages");
+        });
+        test("inital relativePages are empty", function () {
+            QUnit.deepEqual(clientPager.relativePages(), [], "relativePages");
+        });
+        
+        
+        module("paging view model - ClientPager - updates to observableArray");
             
         test("pager updates with observableArray", function(){
             var array = [0, 1, 2, 3, 4, 5];
             observableArray(array);
             expect(2);
-            QUnit.deepEqual(pager.pagedItems(), array, "paged items");
-            QUnit.deepEqual(pager.relativePages(), [1], "relativePages");
+            QUnit.deepEqual(clientPager.pagedItems(), array, "paged items");
+            QUnit.deepEqual(clientPager.relativePages(), [1], "relativePages");
         });
         test("page cannot go below 1", function () {
             expect(2);
             pager.page(0);
-            QUnit.equal(pager.page(), 1, "page");
+            QUnit.equal(clientPager.page(), 1, "page");
             pager.page(-1);
-            QUnit.equal(pager.page(), 1, "page");
+            QUnit.equal(clientPager.page(), 1, "page");
         });
         test("page cannot go past last page with single page of data", function () {
             expect(2);
             pager.page(2);
-            QUnit.equal(pager.page(), 1, "page");
+            QUnit.equal(clientPager.page(), 1, "page");
             pager.page(10);
-            QUnit.equal(pager.page(), 1, "page");
+            QUnit.equal(clientPager.page(), 1, "page");
         });
         test("relativePages only show available pages", function(){
             expect(2);
-            QUnit.deepEqual(pager.relativePages(), [1], "relativePages");
+            QUnit.deepEqual(clientPager.relativePages(), [1], "relativePages");
             var array = ko.utils.range(0, 25);
             observableArray(array);
-            QUnit.deepEqual(pager.relativePages(), [1, 2, 3], "relativePages");
+            QUnit.deepEqual(clientPager.relativePages(), [1, 2, 3], "relativePages");
         });
         test("page cannot go past last page with multiple pages of data", function () {
             var array = ko.utils.range(0,25);
             observableArray(array);
             expect(2);
-            pager.page(4);
-            QUnit.equal(pager.page(), 3, "page");
-            pager.page(10);
-            QUnit.equal(pager.page(), 3, "page");
+            clientPager.page(4);
+            QUnit.equal(clientPager.page(), 3, "page");
+            clientPager.page(10);
+            QUnit.equal(clientPager.page(), 3, "page");
         });
         test("pager updates with observableArray and only displays one page", function(){
             var array = ko.utils.range(0,100);
             observableArray(array);
             expect(2);
-            QUnit.deepEqual(pager.pagedItems(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], "paged items");
-            QUnit.deepEqual(pager.relativePages(), [1, 2, 3, 4, 5], "relativePages");
+            QUnit.deepEqual(clientPager.pagedItems(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], "paged items");
+            QUnit.deepEqual(clientPager.relativePages(), [1, 2, 3, 4, 5], "relativePages");
         });
         test("page can update with multiple pages of data", function () {
             expect(2);
-            pager.page(2);
-            QUnit.equal(pager.page(), 2, "page");
-            pager.page(3);
-            QUnit.equal(pager.page(), 3, "page");
+            clientPager.page(2);
+            QUnit.equal(clientPager.page(), 2, "page");
+            clientPager.page(3);
+            QUnit.equal(clientPager.page(), 3, "page");
         });
         test("changing the page updates the relativePages", function () {
             expect(2);
-            pager.page(5);
-            QUnit.deepEqual(pager.relativePages(), [3, 4, 5, 6, 7], "relativePages");
-            pager.page(11);
-            QUnit.deepEqual(pager.relativePages(), [7, 8, 9, 10, 11], "relativePages");
+            clientPager.page(5);
+            QUnit.deepEqual(clientPager.relativePages(), [3, 4, 5, 6, 7], "relativePages");
+            clientPager.page(11);
+            QUnit.deepEqual(clientPager.relativePages(), [7, 8, 9, 10, 11], "relativePages");
         });
         
         
